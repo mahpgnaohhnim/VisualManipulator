15503b946b43f564538734e6666a3b8a2f5632e9
Henry Pham <h.pham@pripares.com>
2020-08-26 17:17:24 +0200

    #hp26Aug2020 Updates

---
+++ precache-manifest.be2b2495b694b3a54bdd90a3b791a282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1425d3be3731a9e6ce97203d51db0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce0a6596e417d1ac49f4",
+    "url": "/static/css/main.7addaeae.chunk.css"
+  },
+  {
+    "revision": "1ec5ea8a411d42c40d03",
+    "url": "/static/js/2.86c0cf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86c0cf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0a6596e417d1ac49f4",
+    "url": "/static/js/main.1bd437eb.chunk.js"
+  },
+  {
+    "revision": "f157b4c9fb5014291a7e",
+    "url": "/static/js/runtime-main.3c5fb2fa.js"
+  }
+]);
